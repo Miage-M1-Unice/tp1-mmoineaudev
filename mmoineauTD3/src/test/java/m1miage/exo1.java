b2273cec4198c0b7d451a414308098e1c6cedf90
Maxime M <maxime.moineau@free.fr>
2019-03-12 08:48:35 +0100

    add tp3 exo1

--- mmoineauTD3/src/test/java/m1miage/exo1.java
+++ mmoineauTD3/src/test/java/m1miage/exo1.java
@@ -1,9 +1,13 @@
 package m1miage;
 
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLClassLoader;
 
 /**
@@ -30,8 +34,44 @@ public class exo1
     {
         ClassLoader ucl = this.getClass().getClassLoader();
 
-        //FROM TP1
-        ucl.loadClass()
+        /*
+        on essaie de récupérer
+        file:///home/asus/Desktop/work/prog_avancee_huet/TP2/target/classes/MIAGE/introspection/AnalyseurDeClasse.class
+         */
+        try {
+            ucl.loadClass("MIAGE.introspection.AnalyseurDeClasse");
+        } catch (ClassNotFoundException e) {
+            assertTrue( true );
+        }
+
+    }
+
+    @Test
+    public void shouldSucceed()
+    {
+        URL url = null;
+        try {
+            url = new URL("file:///home/asus/Desktop/work/prog_avancee_huet/TP2/target/TP2-1.0-SNAPSHOT.jar");
+        } catch (MalformedURLException e) {
+            fail();
+        }
+        URL[] urlArray = {url};
+        URLClassLoader ucl = new URLClassLoader(urlArray, this.getClass().getClassLoader());
+
+        /*
+        on essaie de récupérer
+        MIAGE/introspection/AnalyseurDeClasse.class
+         */
+        try {
+            Class classLoaded = ucl.loadClass("MIAGE.introspection.AnalyseurDeClasse");
+            Method[] m = classLoaded.getMethods();
+            System.out.println("From TP2 : méthodes de MIAGE.introspection.AnalyseurDeClasse");
+            for(Method aMethod : m){
+                System.out.println(aMethod.getName());
+            }
+        } catch (ClassNotFoundException e) {
+            fail();
+        }
 
         assertTrue( true );
     }
