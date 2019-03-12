bc59e2013e26f6641eac7b2c76f6f80783b42469
Maxime M <maxime.moineau@free.fr>
2019-03-12 09:56:26 +0100

    illegal name...

--- mmoineauTD3/src/main/java/m1miage/MyClassLoader.java
+++ mmoineauTD3/src/main/java/m1miage/MyClassLoader.java
@@ -6,19 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyClassLoader extends SecureClassLoader {
-    private ArrayList<File> path = null;
 
-    public static ArrayList<File> getPaths() {
-        ArrayList res = new ArrayList<File>();
-        res.add(new File("/home/asus/Desktop/work/prog_avancee_huet/TP2/target/classes/MIAGE/introspection/ClasseBidonBisBis.class"));
-        res.add(new File("/home/asus/Desktop/work/prog_avancee_huet/TP2/target/classes/MIAGE/introspection/ClasseBidonBis.class"));
-        res.add(new File("/home/asus/Desktop/work/prog_avancee_huet/TP2/target/classes/MIAGE/introspection/ClasseBidon.class"));
-        res.add(new File("/home/asus/Desktop/work/prog_avancee_huet/TP2/target/classes/MIAGE/introspection/AnalyseurDeClasse.class"));
-        return res;
-    }
+    private String path;
 
-    public MyClassLoader(ArrayList<File> p) {
-        this.path = p;
+    public MyClassLoader(String s) {
+        this.path = s;
     }
 
 
@@ -31,26 +23,17 @@ public class MyClassLoader extends SecureClassLoader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return (Class<?>) super.defineClass(getName(name), b, 0, b.length);
-    }
-
-    private String getName(String name) {
-
-        String res = name.substring(name.indexOf("classes/"), name.length())
-                .replace("classes/", "")
-                .replace(".class", "")
-                .replace("/", ".");
-
-        MIAGE_M1.view.Console.debug(this, "getName :"+res);
+        Class<?> res = super.defineClass(name, b, 0, b.length);
         return res;
-    }
 
+    }
 
     int i=0;
     private byte[] loadClassData(String name) throws ClassNotFoundException, IOException {
         MIAGE_M1.view.Console.debug(this, "loadClassData :"+name);
 
         DataInputStream inputStream = new DataInputStream(new FileInputStream(name));
+
         List<Byte> resList = new ArrayList<Byte>();
         while(inputStream.available()>0){
             resList.add(inputStream.readByte());
--- mmoineauTD3/src/test/java/m1miage/exo2.java
+++ mmoineauTD3/src/test/java/m1miage/exo2.java
@@ -3,25 +3,30 @@ package m1miage;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static m1miage.MyClassLoader.getPaths;
 import static org.junit.Assert.*;
 
 public class exo2 {
 
 
     @Test
-    public void myClassLoaderTest(){
+    public void myClassLoaderTest() throws IOException {
 
-        ArrayList<File> al = getPaths();
+        ArrayList<File> al = getPaths("/home/asus/Desktop/work/prog_avancee_huet/TP2/target/classes");
         ArrayList<Class> classes = new ArrayList<>();
-        MyClassLoader myCL = new MyClassLoader(al);
+        MyClassLoader myCL = null;
 
         try {
-            for(File f : al){
-                classes.add(myCL.findClass(f.getPath()));
+            for(File fileInList : al){
+                myCL = new MyClassLoader(fileInList.getPath());
+                classes.add(myCL.findClass(fileInList.getPath()));
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -30,5 +35,15 @@ public class exo2 {
         for(Class c : classes){
             MIAGE_M1.view.Console.debug(this, c.getSimpleName());
         }
+    }
+
+    private ArrayList<File> getPaths(String aPath) {
+        ArrayList<File> al = new ArrayList();
+        File classDir = new File(aPath);
+        for(File f : classDir.listFiles()) {
+            if(f.isDirectory()) al.addAll(getPaths(f.getPath()));
+            else al.add(f);
+        }
+        return al;
     }
 }
